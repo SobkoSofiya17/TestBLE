641acff85f073833dbc923dfd18c6b5602b34dcd
SobkoSofiya17 <sobkosofiya17@gmail.com>
2021-11-29 14:23:11 +0300

    Initial Commit

---
+++ TestBLE/TestBLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBLEApp.swift
+//  TestBLE
+//
+//  Created by Тест on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestBLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
